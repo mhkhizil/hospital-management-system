7cb3bfd72b46f0de6187a55a21d6a0a88fd629a5
Min Thuta Saw Naing Eric <minthutasawnaing@yahoo.com>
2025-12-10 19:36:04 +0630

    Add new API origin to connectSources

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,7 @@ const connectSources = Array.from(
   new Set([
     "'self'",
     apiOrigin,
+    "https://api.nationalcancercenter.click",
     "http://127.0.0.1:8000",
     "http://localhost:8000",
     "https:",
